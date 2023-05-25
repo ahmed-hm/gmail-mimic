28dd5d282d24643d79b6d2029966760cff328f50
Ahmed Hamdy <hamdimn54@gmail.com>
2023-05-25 22:42:53 +0200

    [Backend] updated barrel file in shared folder

--- Backend/src/shared/index.ts
+++ Backend/src/shared/index.ts
@@ -1,6 +1,5 @@
 export * from './constants';
 export * from './decorators';
 export * from './entities';
-export * from './error';
 export * from './exception-handlers';
 export * from './response';
