17871a162d4a2d81e7e2bae85ade146fec85f127
Ahmed Hamdy <hamdimn54@gmail.com>
2023-05-25 22:24:59 +0200

    [Backend] - update app module - update application bootstrap

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,10 +1,54 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER, APP_GUARD } from '@nestjs/core';
+import { InjectModel, MongooseModule } from '@nestjs/mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { AuthModule } from './modules/auth/auth.module';
+import { JWTGuard } from './modules/auth/jwt/jwt.guard';
+import { EmailModule } from './modules/email/email.module';
+import { EmailModel, emailModelName } from './modules/email/schema/email.schema';
+import { UserModel, userModelName } from './modules/user/schema/user.schema';
+import { UserModule } from './modules/user/user.module';
+import { GlobalHandler } from './shared';
+import * as seed from './shared/seed/seed.json';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    EmailModule,
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        const mongod = await MongoMemoryServer.create({ instance: { port: 4444 } });
+        const uri = mongod.getUri();
+
+        return { uri };
+      },
+    }),
+  ],
+  controllers: [],
+  providers: [
+    { provide: APP_FILTER, useClass: GlobalHandler },
+    { provide: APP_GUARD, useClass: JWTGuard },
+  ],
 })
-export class AppModule {}
+export class AppModule {
+  logger = new Logger(AppModule.name);
+  constructor(
+    @InjectModel(userModelName) private userModel: UserModel,
+    @InjectModel(emailModelName) private emailModel: EmailModel,
+  ) {
+    this.seedData();
+  }
+
+  async seedData() {
+    this.logger.log('starting seed');
+
+    const users = seed.users.map(async (user) => await this.userModel.create(user));
+    const emails = seed.emails.map(async (email) => await this.emailModel.create(email));
+
+    await Promise.all([...users, ...emails]);
+    this.logger.log('finished seed');
+  }
+}
--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,8 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  const config = new DocumentBuilder().setTitle('Emails API').setVersion('1.0').addBearerAuth().build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: { persistAuthorization: true },
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
